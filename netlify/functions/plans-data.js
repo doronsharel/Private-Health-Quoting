0d85b7cada1eec2c04955b5f2be30a54a37c47bd
Doron Sharel <doronsharel@Dorons-Mac-mini.local>
2025-12-19 16:25:58 -0500

    Add Med Max 1000 and 1500 List of Covered Services PDFs (using actual filenames with typos)

--- netlify/functions/plans-data.js
+++ netlify/functions/plans-data.js
@@ -503,7 +503,7 @@ module.exports = [
     planTag: "vl",
     badge: "First Health Network",
     pdf: "pdfs/Med Max.pdf",
-    eocPdf: "COMING_SOON", // Coming Soon - PDF not yet available
+    eocPdf: "pdfs/MEX MAX 1000 FIRST HEALTH List of Covered Services.pdf", // Evidence of Coverage PDF (note: filename has typo "MEX")
     doctorSearchUrl: "https://www.myfirsthealth.com/", // Doctor search URL
     enrollUrl:
       "https://www.1enrollment.com/index.cfm?id=938160&cat=MedMax%20-%20DVP",
@@ -569,7 +569,7 @@ module.exports = [
     planTag: "vl",
     badge: "First Health Network",
     pdf: "pdfs/Med Max.pdf",
-    eocPdf: "COMING_SOON", // Coming Soon - PDF not yet available
+    eocPdf: "pdfs/MED MEAX 1500 FIRST HEALTH List of Covered Services.pdf", // Evidence of Coverage PDF (note: filename has typo "MEAX")
     doctorSearchUrl: "https://www.myfirsthealth.com/", // Doctor search URL
     enrollUrl:
       "https://www.1enrollment.com/index.cfm?id=938160&cat=MedMax%20-%20DVP",
